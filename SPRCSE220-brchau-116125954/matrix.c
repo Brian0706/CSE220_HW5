2a9d2ef979c7aab74c62ab64f7849c418d5384b0
Brian Chau <brianchau0729@gmail.com>
2026-03-17 14:39:21 -0400

    Finished Sparce Matrix

--- SPRCSE220-brchau-116125954/matrix.c
+++ SPRCSE220-brchau-116125954/matrix.c
@@ -9,6 +9,7 @@
 
 #include "matrix.h"
 #include <stdio.h>
+#include <string.h>
 
 int max(int a, int b)
 {
@@ -27,72 +28,93 @@ int SparseMatrix(int D[2], int M[D[0]][D[1]],
     int rows = D[0];
     int cols = D[1];
     int m = max(rows, cols);
+    /*Used to store the value at a certain (row,col) of M*/
+    int value;
 
     int nonZeroCount = 0;
+    /*Initialize the array S with 0*/
+    memset(S, 0, 3 * m * sizeof(int));
 
-    return 0;
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < cols; j++){
+        value = *(*(M + i) + j);
+        if(value){
+          if(nonZeroCount > m){
+            return -1;
+          }
+          /*Used to set value of S, S's avlues are set even if there
+           *the matrix is not sparce.
+          */
+          *(*(S)+nonZeroCount) = i;
+          *(*(S+1)+nonZeroCount) = j;
+          *(*(S+2)+nonZeroCount) = value;
+          nonZeroCount++;
+        }
+      }
+    }
+    return nonZeroCount;
 }
 
 
-/*----------------------------------------------------
-  2. Hadamard Product
-----------------------------------------------------*/
+// /*----------------------------------------------------
+//   2. Hadamard Product
+// ----------------------------------------------------*/
 int HadamardProduct(const int D[6],
                     const int M[D[0]][D[1]],
                     const int N[D[2]][D[3]],
                     int A[D[4]][D[5]])
 {
 
-    int mRows = D[0];
-    int mCols = D[1];
+//     int mRows = D[0];
+//     int mCols = D[1];
 
-    int nRows = D[2];
-    int nCols = D[3];
+//     int nRows = D[2];
+//     int nCols = D[3];
 
-    int aRows = D[4];
-    int aCols = D[5];
+//     int aRows = D[4];
+//     int aCols = D[5];
 
 
     return 0;
 }
 
 
-/*----------------------------------------------------
-  3. Matrix Multiplication
-----------------------------------------------------*/
+// /*----------------------------------------------------
+//   3. Matrix Multiplication
+// ----------------------------------------------------*/
 int Multiplication(const int D[6],
                    const int M[D[0]][D[1]],
                    const int N[D[2]][D[3]],
                    int A[D[4]][D[5]])
 {
 
-    int mRows = D[0];
-    int mCols = D[1];
+//     int mRows = D[0];
+//     int mCols = D[1];
 
-    int nRows = D[2];
-    int nCols = D[3];
+//     int nRows = D[2];
+//     int nCols = D[3];
 
-    int aRows = D[4];
-    int aCols = D[5];
+//     int aRows = D[4];
+//     int aCols = D[5];
 
 
     return 0;
 }
 
 
-/*----------------------------------------------------
-  4. Diagonal Sum
-----------------------------------------------------*/
+// /*----------------------------------------------------
+//   4. Diagonal Sum
+// ----------------------------------------------------*/
 int DiagonalSum(const int D[4],
                 const int A[D[0]][D[1]],
                 int DS[D[2]][D[3]])
 {
 
-    int rows = D[0];
-    int cols = D[1];
+//     int rows = D[0];
+//     int cols = D[1];
 
-    int dsRows = D[2];
-    int dsCols = D[3];
+//     int dsRows = D[2];
+//     int dsCols = D[3];
 
 
     return 0;
--- SPRCSE220-brchau-116125954/test_matrix.c
+++ SPRCSE220-brchau-116125954/test_matrix.c
@@ -19,61 +19,61 @@ Test(SparseMatrix, simple_sparse)
     cr_assert_eq(result,3);
 }
 
-Test(HadamardProduct, compatible)
-{
-    int D[6] = {2,2,2,2,2,2};
+// Test(HadamardProduct, compatible)
+// {
+//     int D[6] = {2,2,2,2,2,2};
 
-    int M[2][2] = {
-        {1,2},
-        {3,4}
-    };
+//     int M[2][2] = {
+//         {1,2},
+//         {3,4}
+//     };
 
-    int N[2][2] = {
-        {2,2},
-        {2,2}
-    };
+//     int N[2][2] = {
+//         {2,2},
+//         {2,2}
+//     };
 
-    int A[2][2];
+//     int A[2][2];
 
-    int r = HadamardProduct(D,M,N,A);
+//     int r = HadamardProduct(D,M,N,A);
 
-    cr_assert_eq(r,1);
-}
+//     cr_assert_eq(r,1);
+// }
 
-Test(Multiplication, simple_case)
-{
-    int D[6] = {2,2,2,2,2,2};
+// Test(Multiplication, simple_case)
+// {
+//     int D[6] = {2,2,2,2,2,2};
 
-    int M[2][2] = {
-        {1,2},
-        {3,4}
-    };
+//     int M[2][2] = {
+//         {1,2},
+//         {3,4}
+//     };
 
-    int N[2][2] = {
-        {5,6},
-        {7,8}
-    };
+//     int N[2][2] = {
+//         {5,6},
+//         {7,8}
+//     };
 
-    int A[2][2];
+//     int A[2][2];
 
-    int r = Multiplication(D,M,N,A);
+//     int r = Multiplication(D,M,N,A);
 
-    cr_assert_eq(r,1);
-}
+//     cr_assert_eq(r,1);
+// }
 
-Test(DiagonalSum, square_matrix)
-{
-    int D[4] = {3,3,5,5};
+// Test(DiagonalSum, square_matrix)
+// {
+//     int D[4] = {3,3,5,5};
 
-    int A[3][3] = {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-    };
+//     int A[3][3] = {
+//         {1,2,3},
+//         {4,5,6},
+//         {7,8,9}
+//     };
 
-    int DS[5][5];
+//     int DS[5][5];
 
-    int r = DiagonalSum(D,A,DS);
+//     int r = DiagonalSum(D,A,DS);
 
-    cr_assert_eq(r,1);
-}
+//     cr_assert_eq(r,1);
+// }
