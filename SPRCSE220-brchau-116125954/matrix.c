9270c754e6f459e5e3972d22717f271b73d60f83
Brian Chau <brianchau0729@gmail.com>
2026-03-19 11:48:15 -0400

    Did all four functions, need to do further tests

--- SPRCSE220-brchau-116125954/matrix.c
+++ SPRCSE220-brchau-116125954/matrix.c
@@ -18,6 +18,13 @@ int max(int a, int b)
     return b;
 }
 
+int min(int a, int b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
 /*----------------------------------------------------
   1. Sparse Matrix
 ----------------------------------------------------*/
@@ -65,17 +72,57 @@ int HadamardProduct(const int D[6],
                     int A[D[4]][D[5]])
 {
 
-//     int mRows = D[0];
-//     int mCols = D[1];
+    int mRows = D[0];
+    int mCols = D[1];
 
-//     int nRows = D[2];
-//     int nCols = D[3];
+    int nRows = D[2];
+    int nCols = D[3];
 
-//     int aRows = D[4];
-//     int aCols = D[5];
+    int aRows = D[4];
+    int aCols = D[5];
 
+    int rows;
+    int cols;
 
-    return 0;
+    int status;
+    int mVal;
+    int nVal;
+    if(mRows == nRows && mCols == nCols){
+      rows = mRows;
+      cols = nCols;
+      if(aRows < rows || aCols < cols){
+        rows = aRows;
+        cols = aCols;
+        status = -3;
+      }
+      else if(aRows > rows || aCols > cols){
+        status = 2;
+      }
+      else{
+        status = 1;
+      }
+    }
+    else{
+      rows = min(mRows,nRows);
+      cols = min(mCols,nCols);
+      if(aRows < rows || aCols < cols){
+        rows = aRows;
+        cols = aCols;
+        status = -2;
+      }
+      else{
+        status = -1;
+      }
+    }
+    memset(A, 0, aRows * aCols * sizeof(int));
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < cols; j++ ){
+        mVal = *(*(M + i) + j);
+        nVal = *(*(N + i) + j);
+        *(*(A + i) + j) = mVal * nVal;
+      }
+    }
+    return status;
 }
 
 
@@ -88,17 +135,57 @@ int Multiplication(const int D[6],
                    int A[D[4]][D[5]])
 {
 
-//     int mRows = D[0];
-//     int mCols = D[1];
+    int mRows = D[0];
+    int mCols = D[1];
 
-//     int nRows = D[2];
-//     int nCols = D[3];
+    int nRows = D[2];
+    int nCols = D[3];
 
-//     int aRows = D[4];
-//     int aCols = D[5];
+    int aRows = D[4];
+    int aCols = D[5];
 
-
-    return 0;
+    int rows = mRows;
+    int cols = nCols;
+    /*Size of the rows and cols we will multiply*/
+    int length;
+    int status;
+    int value;
+    if(mCols != nRows){
+      length = min(rows,cols);
+      if(aRows < rows || aCols < cols){
+        rows = aRows;
+        cols = aCols;
+        status = -1;
+      }
+      else{
+        status = -2;
+      }
+    }
+    else{
+      length = mCols;
+      if(aRows < rows || aCols < cols){
+        rows = aRows;
+        cols = aCols;
+        status = -3;
+      }
+      else if(aRows > rows || aCols > cols){
+        status = 2;
+      }
+      else{
+        status = 1;
+      }
+    }
+    memset(A, 0, aRows * aCols * sizeof(int));
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < cols; j++){
+        value = 0;
+        for(int n = 0; n < length; n++){
+          value += *(*(M+i)+n) * *(*(N+n)+j);
+        }
+        *(*(A+i)+j) = value;
+      }
+    }
+    return status;
 }
 
 
@@ -110,12 +197,52 @@ int DiagonalSum(const int D[4],
                 int DS[D[2]][D[3]])
 {
 
-//     int rows = D[0];
-//     int cols = D[1];
+    int rows = D[0];
+    int cols = D[1];
 
-//     int dsRows = D[2];
-//     int dsCols = D[3];
-
-
-    return 0;
+    int dsRows = D[2];
+    int dsCols = D[3];
+    
+    int status;
+    int value;
+    int diagonalSize = min(rows,cols);
+    /*Counter variable for for loops*/
+    int i;
+    memset(DS, 0, dsRows * dsCols * sizeof(int));
+    if(dsRows < rows + 2 || dsCols < cols + 2){
+      if(dsRows < 1){
+        return -1;
+      }
+      status = -1;
+    }
+    else if(dsRows > rows + 2 || dsCols > cols + 2){
+      status = 2;
+    }
+    else{
+      status = 1;
+    }
+    for(i = 0; i < diagonalSize; i++){
+      value += *(*(A+i)+i);
+    }
+    *(*DS) = value;
+    value = 0;
+    for(i = 0; i < diagonalSize;i++){
+      value += *(*(A+i)+(cols-i-1));
+    }
+    *(*(DS)+1) = value;
+    for(i = 0; i < dsCols;i++){
+      value = 0;
+      for(int j = 0; j < rows; j++){
+        value += *(*(A+j)+i);
+      }
+      *(*(DS+1)+i) = value;
+    }
+    for(i = 2; i < dsRows;i++){
+      value = 0;
+      for(int j = 0; j < cols; j++){
+        value += *(*(A+i)+j);
+      }
+      *(*(DS+i)) = value;
+    }
+    return status;
 }
